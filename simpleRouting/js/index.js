9b0a40f6f761d3600e11641d9fe8899ade57faf4
Fairfarren <lkywow@live.com>
2017-05-26 00:42:15 +0800

    simpleRouting
    之前的代码不能往兄弟的子级跳转，只能在子级跳转。用了一个for循环去把目前的hash和过去的hash一一对应比较，不一样的话就去重新加载。加载到
    有默认加载模块的地方，就去gbl下面查找nowLocation的长度。长度如果大于当前的层数，那就不加载默认的。因为点击的层数大于默认加载的层级
    ，默认加载的层级会把nowLocation覆盖掉，最后加载出来的还是默认样式

--- simpleRouting/js/index.js
+++ simpleRouting/js/index.js
@@ -1,7 +1,8 @@
 var gbl = gbl || {};
 //头部
-gbl.header = '<div>header<a href="#/header/home1" onclick="gbl.changeModle1(\'#/header/home1\',\'2\')">home1</a><a href="#/header/home2" onclick="gbl.changeModle1(\'#/header/home2\',\'2\');">home2</a><div class="model2"></div></div><script>'+
+gbl.header = '<div>header<a href="javascript:(0)" onclick="gbl.changeModle1(\'#/header/home1\',\'2\')">home1</a><a href="javascript:(0);" onclick="gbl.changeModle1(\'#/header/home2\',\'2\');">home2</a><div class="model2"></div></div><script>'+
 	'$(document).ready(function(){'+
+		'if(gbl.nowLocation.length > 3){return};'+
 		'var locationHash = window.location.hash.split("/").splice(0,3);'+
 		'if(locationHash.length>=3){'+
 			'var locationHashArr = locationHash.join(\'/\');'+
@@ -12,10 +13,11 @@ gbl.header = '<div>header<a href="#/header/home1" onclick="gbl.changeModle1(\'#/
 	'})'+
 '</script>';
 //尾部
-gbl.footer = '<div>footer</div>';
+gbl.footer = '<div>footer<a href="javascript:(0)" onclick="gbl.changeModle1(\'#/header/home1\',\'2\')">home1</a><a href="javascript:(0);" onclick="gbl.changeModle1(\'#/header/home2\',\'2\');">home2</a><a href="javascript:(0);" onclick="gbl.changeModle1(\'#/header/home2/home3\',\'3\');">home3</a></div>';
 
 gbl.home1 = '<div>home1</div>';
-gbl.home2 = '<div>home2</div>';
+gbl.home2 = '<div>home2<div class="model3"></div></div>';
+gbl.home3 = '<div>home3</div>'
 
 gbl.changeModle1 = function(loca,index){
 	//改变前
@@ -24,17 +26,40 @@ gbl.changeModle1 = function(loca,index){
 			return;
 		}
 	};
+	var theLastWindowHash = window.location.hash.split('/');
 	//改变后
+	window.location.hash = loca;
 	gbl.nowLocation = loca.split('/');
+	console.log(gbl.nowLocation);
+	for(var i=1;i<gbl.nowLocation.length;i++){
+		if(gbl.nowLocation[i] != theLastWindowHash[i]){
+			console.log(gbl.nowLocation[i] +'!='+ theLastWindowHash[i]);
+			var goToHash = gbl.nowLocation.slice(0,i+1).join('/');
+			console.log('拼接'+goToHash);
+			console.log('index='+i);
+			gbl.hashModelFC(i);
+		}else{
+			gbl.hashModelFC(index);
+		}
+	}
+
+
+	// gbl.hashModelFC(index);
+	
+	console.log("zhixing");
+
+}
+gbl.hashModelFC = function(index){
+	// gbl.nowLocation = loca.split('/');
+
 	if(!gbl[gbl.nowLocation[index]]){
+		alert('更新了');
 		window.location.hash = '#/header';
 		location.reload([true]);
 		return;
 	}
 	$('.model'+ index +'').children().remove();
 	$('.model'+ index +'').append(gbl[gbl.nowLocation[index]]);
-	console.log(gbl.nowLocation);
-	console.log("zhixing");
 }
 
 
