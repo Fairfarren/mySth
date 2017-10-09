c53144716b7c31b63cc3acbd7d9f4b0b2668bd2e
Yanke Liu <lykwow@live.com>
2017-10-10 00:02:52 +0800

    获取时间
    添加一个倒计时方法

--- newDate/index.js
+++ newDate/index.js
@@ -32,3 +32,50 @@ console.log(`
 	获取秒速 0-59 getSeconds() : ${date.getSeconds()};
 	
 	`)
+
+
+console.log(`
+倒计时方法：
+
+setInterval(function(){
+     // document.body.innerHTML = getTimes(2017,11,29,16,58,10)
+     console.log( getTimes(2017,11,29,16,58,10));
+},1000)
+
+function getTimes(year,month,day,hour,minute,secends){
+    var times = new Date(year,month-1,day,hour,minute,secends) - new Date();
+    var dd = parseInt(times/1000/60/60/24,10);
+    var hh = parseInt(times/1000/60/60%24,10);
+    var mm = parseInt(times/1000/60%60,10);
+    var ss = parseInt(times/1000%60,10)
+    // console.log(hh,mm,ss);
+    var ssTime = "" + ss;
+    if(ss<10){
+        ssTime = "0"+ss;
+    }
+    ss = ssTime;
+    return "距"+year+"离"+month+"年"+day+"月"+hour+"日，还有"+dd+"日"+hh+"时"+mm+"分"+ss+"秒";
+
+}
+	`)
+
+setInterval(function(){
+     // document.body.innerHTML = getTimes(2017,11,29,16,58,10)
+     console.log( getTimes(2017,11,29,16,58,10));
+},1000)
+
+function getTimes(year,month,day,hour,minute,secends){
+    var times = new Date(year,month-1,day,hour,minute,secends) - new Date();
+    var dd = parseInt(times/1000/60/60/24,10);
+    var hh = parseInt(times/1000/60/60%24,10);
+    var mm = parseInt(times/1000/60%60,10);
+    var ss = parseInt(times/1000%60,10)
+    // console.log(hh,mm,ss);
+    var ssTime = "" + ss;
+    if(ss<10){
+        ssTime = "0"+ss;
+    }
+    ss = ssTime;
+    return "距"+year+"离"+month+"年"+day+"月"+hour+"日，还有"+dd+"日"+hh+"时"+mm+"分"+ss+"秒";
+
+}
