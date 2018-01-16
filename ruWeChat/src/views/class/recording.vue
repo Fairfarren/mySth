04001ffadedb2a4257d68d9d4599336ac386c32d
Yanke Liu <lykwow@live.com>
2018-01-16 14:32:14 +0800

    备份

--- ruWeChat/src/views/class/recording.vue
+++ ruWeChat/src/views/class/recording.vue
@@ -488,7 +488,7 @@ export default {
       const url = 'http://www.ruketang.com/wechat/'
       this.axios.get('/wx/share', {
         params: {
-          url: `${url}?recordingId=${this.$route.params.id}`
+          url: url
         }
       }).then(res => {
         if (res.data.status_code === 201) {
@@ -528,7 +528,7 @@ export default {
           desc: this.theClass.desc, // 分享描述
           link: this.jssdk.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
           imgUrl: this.theClass.img, // 分享图标
-          type: '', // 分享类型,music、video或link，不填默认为link
+          type: 'link', // 分享类型,music、video或link，不填默认为link
           dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
           success: function () {
             // 用户确认分享后执行的回调函数
