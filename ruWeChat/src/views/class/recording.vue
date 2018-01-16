7732f68f4d5a1afe0c934552cde96fb5469f46b4
Yanke Liu <lykwow@live.com>
2018-01-16 10:15:17 +0800

    备份

--- ruWeChat/src/views/class/recording.vue
+++ ruWeChat/src/views/class/recording.vue
@@ -487,7 +487,7 @@ export default {
     weChatShare () {
       this.axios.get('/wx/share', {
         params: {
-          url: window.location.href.split('#')[0]
+          url: `https://www.ruketang.wechat?recordingId=${this.$route.params.id}`
         }
       }).then(res => {
         if (res.data.status_code === 201) {
@@ -511,27 +511,32 @@ export default {
         signature: 'MD5', // 必填，签名，见附录1
         jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       })
-      wx.onMenuShareTimeline({
-        title: this.theClass.name, // 分享标题
-        link: this.jssdk.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '', // 分享图标
-        success: function () {
-          // 用户确认分享后执行的回调函数
-        }
-      })
-      wx.onMenuShareAppMessage({
-        title: this.theClass.name, // 分享标题
-        desc: this.theClass.desc, // 分享描述
-        link: this.jssdk.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '', // 分享图标
-        type: '', // 分享类型,music、video或link，不填默认为link
-        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-        success: function () {
-        // 用户确认分享后执行的回调函数
-        },
-        cancel: function () {
-        // 用户取消分享后执行的回调函数
-        }
+      wx.ready(() => {
+        wx.onMenuShareTimeline({
+          title: this.theClass.name, // 分享标题
+          link: this.jssdk.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: this.theClass.img, // 分享图标
+          success: function () {
+            // 用户确认分享后执行的回调函数
+            alert('分享成功')
+          }
+        })
+        wx.onMenuShareAppMessage({
+          title: this.theClass.name, // 分享标题
+          desc: this.theClass.desc, // 分享描述
+          link: this.jssdk.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: this.theClass.img, // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            // 用户确认分享后执行的回调函数
+            alert('分享成功')
+          },
+          cancel: function (error) {
+            // 用户取消分享后执行的回调函数
+            alert('失败' + error)
+          }
+        })
       })
     }
   },
